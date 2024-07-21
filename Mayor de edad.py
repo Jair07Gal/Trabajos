ad82911e3dde47e43d67f743d1fb29858d3dd9b0
Jair07Gal <145640891+Jair07Gal@users.noreply.github.com>
2024-07-21 11:25:18 -0600

    Add files via upload

---
+++ Mayor de edad.py
@@ -0,0 +1,17 @@
+# if else
+edad=18
+if edad >= 18:
+    print ("Es mayor de edad")
+else: 
+    print ("No es mayor de edad")
+
+
+# if elif else 
+ 
+if edad >= 18:
+    print ("Es mayor de edad")
+elif edad == 15:
+    print ("No es igual a 15")
+else :
+    print ("No es mayor de edad")
+    
